083ebe9fbd69e50d6aa0304a68dbe61a196d0255
P1T0NN <ognjen.tapuskovic@gmail.com>
2026-08-14 13:51:48 +0200

    Added tab icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '@/shared/lib/assets/favicon.svg';
+	import favicon from '/favicon.ico';
 
 	// COMPONENTS
 	import { Toaster } from '@/shared/components/ui/sonner';
